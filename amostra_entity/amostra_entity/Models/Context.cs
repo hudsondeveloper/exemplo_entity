62c46d6f4d859d5f72d4982101eb331b2d256080
Hudson Luiz da Silva Costa <hudson_hip.hop12@hotmail.com>
2018-07-23 03:27:30 -0300

    final

--- amostra_entity/amostra_entity/Models/Context.cs
+++ amostra_entity/amostra_entity/Models/Context.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 
@@ -15,14 +17,45 @@ namespace amostra_entity.Models
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
     
-        public Context() : base("name=Context")
+        public Context() : base("name=amostra_entity")
         {
         }
-
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Console.WriteLine("Entidade do tipo \"{0}\" no estado \"{1}\" tem os seguintes erros de validação:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Erro: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+        }
         public System.Data.Entity.DbSet<amostra_entity.Models.Brinquedo> Brinquedoes { get; set; }
 
         public System.Data.Entity.DbSet<amostra_entity.Models.Crianca> Criancas { get; set; }
 
         public System.Data.Entity.DbSet<amostra_entity.Models.Loja> Lojas { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //modelBuilder.Properties<string>().Configure(c => c.HasMaxLength(250));
+
+          //  modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
